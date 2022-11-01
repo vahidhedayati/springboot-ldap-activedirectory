c356065b7514fa43d99c85a32529f741beb94919
Vahid Hedayati <weaselpause@gmail.com>
2022-11-01 13:17:03 +0000

    tomcat ready

--- src/main/java/com/example/ldap/activedirectory/SpringbootLdapActivedirectoryApplication.java
+++ src/main/java/com/example/ldap/activedirectory/SpringbootLdapActivedirectoryApplication.java
@@ -2,9 +2,17 @@ package com.example.ldap.activedirectory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootLdapActivedirectoryApplication {
+public class SpringbootLdapActivedirectoryApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder builder) {
+		return builder.sources(SpringbootLdapActivedirectoryApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootLdapActivedirectoryApplication.class, args);
